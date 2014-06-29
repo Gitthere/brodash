1a3ee03a050ea7b0125a3803552b16217c4a03de
Alex Conway <alexconway808@gmail.com>
2014-06-28 16:23:43 -1000

    refactored the compact method to push instead of splice the original input array

--- brodash.js
+++ brodash.js
@@ -5,15 +5,15 @@ var _ = {};
 // Creates an array with all falsey values removed. The values 
 // false, null, 0, "", undefined, and NaN are all falsey.
 
-_.compact = function (ar) {
-  for (var i = ar.length - 1; i >= 0; i--) {
-    if (ar[i] == false) {
-      ar.splice(i,1);
-    };
-  };
-  return ar;
+_.compact = function (ar) { 
+    var output = [];
+    for (var i = ar.length - 1; i >= 0; i--) {  
+      if (ar[i] !== 0 && ar[i] !== false && ar[i] !== '') {
+        output.push(ar[i]);
+      }
+    }
+  return output;
 }
-
 
 
 // Creates an array with all falsey values removed. The values 
@@ -25,7 +25,7 @@ _.difference = function (ar) {
 //       ar.splice(i,1);
 //     };
 //   };
-  return [1, 3, 4]; //stub return
+// return [1, 3, 4]; //stub return
 }
 
 module.exports = _;
